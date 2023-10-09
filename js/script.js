edcad150210592d202898405a99f9514b995b915
shaimaa <shaimaa.salah79@gmail.com>
2023-10-09 21:28:44 +0200

    terminer permier part

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function setIncrease() {
     currentValue = currentValue +1;
     document.getElementById("count").innerHTML = currentValue;
   }
-  document.getElementById("ajout").addEventListener("click", setIncrease)
+  document.getElementById("ajout").addEventListener("click", setIncrease);
   
 
   function setDecrease() {
@@ -13,12 +13,22 @@ function setIncrease() {
   }
   document.getElementById("soustraction").addEventListener("click", setDecrease)
   
-
   function setReset() {
     currentValue = 0;
     document.getElementById("count").innerHTML = currentValue;
   }
   document.getElementById("réinitialiser").addEventListener("click", setReset)
+
+  function displayValue() {
+    if (value < 0) {
+    document.getElementById(“value”).style.color = "red";
+    } else if (value > 0) {
+    document.getElementById(“value”)..style.color = "green";
+    } else {
+    document.getElementById(“value”).style.color = "black";
+    }
+    document.getElementById(“value”).textContent = value;
+    }
   
  
   
